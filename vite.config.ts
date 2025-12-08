eea0e5a6663a07d31b66e8d831916d04e99447a5
ashistuc <shuvampanda@yahoo.com>
2025-12-08 12:49:20 +0530

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/odisha-ai-summit-2025v2/"
+      base: "./",
       server: {
         port: 3000,
         host: '0.0.0.0',
