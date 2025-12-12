5f6c8945d829a4aa9304f511acbaa9ebb37e2ae8
Abhishek Sahay <Abhishek.sahay103@mail.com>
2025-12-12 13:35:15 +0530

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ export const Footer: React.FC = () => {
             </p>
             <div className="flex gap-4">
               {/* X (Twitter) */}
-              <a href="#" className="w-10 h-10 rounded-full bg-white/5 border border-white/10 flex items-center justify-center text-gray-400 hover:text-white hover:bg-neon-orange hover:border-neon-orange transition-all hover:shadow-[0_0_15px_rgba(255,107,53,0.5)]">
+              <a href="x.com/EIT_Odisha" className="w-10 h-10 rounded-full bg-white/5 border border-white/10 flex items-center justify-center text-gray-400 hover:text-white hover:bg-neon-orange hover:border-neon-orange transition-all hover:shadow-[0_0_15px_rgba(255,107,53,0.5)]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                 </svg>
